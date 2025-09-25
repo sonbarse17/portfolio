bce4c2898cd9565bc5010dc5fa26077e12e8e1f0
sonbarse17 <sushantsonbarse90@gmail.com>
2025-09-26 00:23:57 +0530

    fixed

--- pages/index.js
+++ pages/index.js
@@ -70,10 +70,10 @@ export default function Home() {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
         
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/assets/images/profile.jpg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/profile.jpg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/profile.jpg" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/profile.jpg" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#2563eb" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
